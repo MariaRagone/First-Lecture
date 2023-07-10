6504e21b89b11f7ecc714708735902bc252f6c0a
Maria Ragone <maria@ragone.net>
2023-07-10 13:41:03 -0400

    added exercises 1-5

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //create a variable called x and give it a value of 5
 
+using System.Transactions;
+
 int age = 5;
 string name = "Justin";
 string name2 = "Ethan";
@@ -46,3 +48,52 @@ else if (oddCheck == 1)
 {
     Console.WriteLine("odd");
 }
+//assignment
+value = 0;
+value = age * 100 - Math.PI;
+
+int assign = 0;
+Console.WriteLine(assign);
+assign += 2;
+
+//null - nothing will appear in the console
+string catName = null;
+Console.WriteLine(catName);
+catName = "Fluffy";
+
+//nonnullable
+//int z = null;
+
+//exercises
+//1
+Console.WriteLine("Example 1: Enter some text.");
+string example1 = Console.ReadLine();
+Console.WriteLine(example1);
+
+//2
+Console.WriteLine("Example 2: Enter a number");
+int example2 = int.Parse(Console.ReadLine());
+example2++;
+Console.WriteLine(example2);
+
+//3
+Console.WriteLine("Example 3: Enter a number.");
+double example3 = double.Parse(Console.ReadLine());
+example3 += .5;
+Console.WriteLine(example3);
+
+//4
+Console.WriteLine("Example 4: Enter the first number");
+double example4 =  double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+double anotherNumberExp4 = double.Parse(Console.ReadLine());
+double sum = example4 + anotherNumberExp4;
+Console.WriteLine($"Your 2 numbers multiply up to: {sum}");
+
+//5
+Console.WriteLine("Example 5: Enter a number");
+double example5 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter another number");
+double anotherNumberExp5 = double.Parse(Console.ReadLine());
+double product = example5 * anotherNumberExp5;
+Console.WriteLine($"Your 2 numbers multiply up to: { product}");
